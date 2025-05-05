637e41a9487a8cb176cb8572313d13b0b249fd14
Koushik <paponkoushik@gmail.com>
2025-05-05 20:52:28 +0600

    wrote test case to get data and checked the pagination is working and also checked the remaining data as well

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -10,6 +10,63 @@ class TaskTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_it_returns_paginated_tasks()
+    {
+        Task::factory()->count(15)->create();
+
+        $response = $this->getJson('/api/tasks?page=1');
+
+        $response->assertOk()
+                ->assertJsonStructure([
+                    'current_page',
+                    'data',
+                    'first_page_url',
+                    'from',
+                    'last_page',
+                    'last_page_url',
+                    'links',
+                    'next_page_url',
+                    'path',
+                    'per_page',
+                    'prev_page_url',
+                    'to',
+                    'total',
+                ]);
+
+        $this->assertCount(10, $response->json('data'));
+    }
+
+    public function test_it_returns_remaining_tasks_on_second_page()
+    {
+        Task::factory()->count(15)->create();
+
+        $response = $this->getJson('/api/tasks?page=2');
+
+        $response->assertOk()
+                ->assertJsonStructure([
+                    'current_page',
+                    'data',
+                    'first_page_url',
+                    'from',
+                    'last_page',
+                    'last_page_url',
+                    'links',
+                    'next_page_url',
+                    'path',
+                    'per_page',
+                    'prev_page_url',
+                    'to',
+                    'total',
+                ])
+                ->assertJson([
+                    'current_page' => 2
+                ]);
+
+        $this->assertCount(5, $response->json('data'));
+    }
+
+
+
     public function test_it_creates_task_with_valid_data()
     {
         $data = ['title' => 'Test Task', 'body' => 'Test body'];
