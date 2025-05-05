288ed7026e31122483498ce205107d0dd408c978
Koushik <paponkoushik@gmail.com>
2025-05-05 20:51:21 +0600

    factory created for task and use it on model

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+        ];
+    }
+}
