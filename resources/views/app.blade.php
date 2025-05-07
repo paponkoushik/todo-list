2adad885031bd44334f5113e32d8968acbd43a46
Koushik <paponkoushik@gmail.com>
2025-05-07 22:13:04 +0600

    title changed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel + Vue 3 App</title>
+    <title>TODO List</title>
     @vite(['resources/js/app.js'])
 </head>
 <body>
